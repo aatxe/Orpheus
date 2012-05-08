b17096123b8ace97e72436ae48908578460ac3a2
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-08 19:33:13 -0400

    Added a real script for Charles, and tons of useful shops.

--- scripts/npc/world0/9220020.js
+++ scripts/npc/world0/9220020.js
@@ -22,7 +22,47 @@
 /* 	Charles - FM NPC
  * 	@author Aaron Weiss
 */
+var status = 0;
+
 function start() {
-    cm.sendSimple("Hello");
-    cm.dispose();
+    cm.sendSimple("Hey there, I'm #rCharles#k. What would you like to do today?\r\n#L0#Warrior Armor Shop#l\r\n#L1#Magician Armor Shop#l\r\n#L2#Thief Armor Shop#l\r\n#L3#Bowman Armor Shop#l\r\n#L4#Pirate Armor Shop#l\r\n#L5#Other Shops#l\r\n");
+}
+
+function action(mode, type, selection) {
+    status++;
+    if (mode != 1) {
+        cm.dispose();
+        return;
+    }
+    if (status == 1) {
+        if (selection == 0) {
+            cm.openShop(100100);
+        } else if (selection == 1) {
+            cm.openShop(101101);
+        } else if (selection == 2) {
+            cm.openShop(102102);
+        } else if (selection == 3) {
+            cm.openShop(103103);
+        } else if (selection == 4) {
+            cm.openShop(104104);
+        } else if (selection == 5) {
+            cm.sendSimple("#r#eOther Shops#k\r\n#L0#Weapons Shop#l\r\n#L1#Maple Shop#l\r\n#L2#Timeless Shop#l\r\n#L3#Megaphone Shop#l\r\n#L4#Chair Shop#l\r\n#L5#Utilities Shop#l\r\n");
+        }
+    } else if (status == 2) {
+        if (selection == 0) {
+            cm.openShop(105105);
+        } else if (selection == 1) {
+            cm.openShop(106106);
+        } else if (selection == 2) {
+            cm.openShop(107107);
+        } else if (selection == 3) {
+            cm.openShop(9999993);
+        } else if (selection == 4) {
+            cm.openShop(9999994);
+        } else if (selection == 5) {
+            cm.openShop(9999999);
+        } else {
+            cm.dispose();
+        }
+    }
 }
---
+++ sql/OrpheusShops.sql
@@ -0,0 +1,14 @@
+DELETE FROM `shopitems` WHERE `shopitemid` = '3362';
+DELETE FROM `shopitems` WHERE `shopitemid` = '3363';
+DELETE FROM `shopitems` WHERE `shopitemid` = '3364';
+DELETE FROM `shopitems` WHERE `shopitemid` = '3365';
+DELETE FROM `shopitems` WHERE `shopitemid` = '3366';
+
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('100100', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('101101', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('102102', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('103103', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('104104', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('105105', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('106106', '9220016');
+INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('107107', '9220016');
