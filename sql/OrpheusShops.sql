b0aafac1c041151b4abdec64ed8dd212a7db3709
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-08 20:34:22 -0400

    Added GM items to SQL script for GM shop.

--- sql/OrpheusShops.sql
+++ sql/OrpheusShops.sql
@@ -3,6 +3,9 @@ DELETE FROM `shopitems` WHERE `shopitemid` = '3363';
 DELETE FROM `shopitems` WHERE `shopitemid` = '3364';
 DELETE FROM `shopitems` WHERE `shopitemid` = '3365';
 DELETE FROM `shopitems` WHERE `shopitemid` = '3366';
+INSERT INTO `shopitems` (`shopid`, `itemid`, `price`, `position`, `pitch`) VALUES ('1337', '1102166', '1', '56', '0');
+INSERT INTO `shopitems` (`shopid`, `itemid`, `price`, `position`, `pitch`) VALUES ('1337', '1082252', '1', '57', '0');
+INSERT INTO `shopitems` (`shopid`, `itemid`, `price`, `position`, `pitch`) VALUES ('1337', '1072368', '1', '58', '0');
 
 INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('100100', '9220016');
 INSERT INTO `shops` (`shopid`, `npcid`) VALUES ('101101', '9220016');
