96ceef15202d834db59f2b48c61980f2684b9ce9
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-12 14:49:51 -0400

    Added an SQL script to delete Perfect Pitch shop.

---
+++ src/constants/ScriptableNPCConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+// This file is pretty much here to prevent ServerConstants from getting messy.
+public class ScriptableNPCConstants {
+	// An array of NPC IDs to make scriptable.
+	public static final int[] SCRIPTABLE_NPCS = new int[]{9010000};
+	
+	// An array of descriptions of said NPCs.
+	public static final String[] SCRIPTABLE_NPCS_DESC = new String[]{"Maple Administrator"};
+}
