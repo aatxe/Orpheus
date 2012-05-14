40e098629c07668276955595619721c35c5e6111
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-13 23:32:24 -0400

    Fixed up the broken Aran fix.

--- src/constants/ScriptableNPCConstants.java
+++ src/constants/ScriptableNPCConstants.java
@@ -3,7 +3,7 @@ package constants;
 // This file is pretty much here to prevent ServerConstants from getting messy.
 public class ScriptableNPCConstants {
 	// An array of NPC IDs to make scriptable.
-	public static final int[] SCRIPTABLE_NPCS = new int[]{9010000,1201000};
+	public static final int[] SCRIPTABLE_NPCS = new int[]{9010000, 1202000};
 	
 	// An array of descriptions of said NPCs.
 	public static final String[] SCRIPTABLE_NPCS_DESC = new String[]{"Maple Administrator", "Tutorial Lilin"};
