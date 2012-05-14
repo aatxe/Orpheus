0186b8844ee5775a67a48f1758bfa4902f3d4285
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-13 22:17:23 -0400

    Removed the now unnecessary MapleAdmin.sql

--- sql/MapleAdmin.sql
+++
@@ -1,1 +0,0 @@
-insert into `Orpheus`.`spawns` ( `mid`, `mobtime`, `x`, `idd`, `type`, `fh`, `cy`, `y`, `rx0`, `f`, `rx1`) values ( '0', '1000', '206', '9010000', 'n', '0', '-334', '-334', '256', '0', '156');
