7180265a81c4b7e1eb18a65c43611f86599eecd2
Aaron Weiss <aaronweiss74@gmail.com>
2012-05-22 23:43:32 -0400

    Slight tweak to the default runsrv.bat

--- mksrv.bat
+++ mksrv.bat
@@ -1,5 +1,5 @@
 @echo off
-@title MoopleDEV's INI creator
+@title OrpheusMS Server
 set CLASSPATH=.;dist\*
 java -Xmx10m -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd net.server.CreateINI
 pause
